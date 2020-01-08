06e666ddd455501bb5bca763c99e31e3bb815b6a
Alexandre Bulté <alexandre@bulte.net>
2020-01-08 19:15:02 +0100

    Fix register form by removing lazy_gettext

--- udata/auth/forms.py
+++ udata/auth/forms.py
@@ -1,7 +1,7 @@
 from flask_security.forms import RegisterForm
 from udata.forms import fields
 from udata.forms import validators
-from udata.i18n import lazy_gettext as _
+from udata.i18n import gettext as _
 
 
 class ExtendedRegisterForm(RegisterForm):
